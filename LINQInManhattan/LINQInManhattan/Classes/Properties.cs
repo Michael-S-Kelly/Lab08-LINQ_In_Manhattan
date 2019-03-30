73eeb04d6bba36eb8766e7ba76108cc8da5d6dc9
Michael Kelly <mike.s.kelly@icloud.com>
2019-03-30 13:03:04 -0700

    Save before tutoring

---
+++ LINQInManhattan/LINQInManhattan/Classes/Properties.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQInManhattan.Classes
+{
+    class Properties
+    {
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Address { get; set; }
+        public string Borough { get; set; }
+        public string Neighborhood { get; set; }
+        public string County { get; set; }
+
+    }
+}
--- LINQInManhattan/LINQInManhattan/Program.cs
+++ LINQInManhattan/LINQInManhattan/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using LINQInManhattan.Classes;
 
 namespace LINQInManhattan
 {
@@ -6,7 +10,56 @@ namespace LINQInManhattan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool menu = true;
+            while (menu)
+            {
+                menu = NavMenu();
+            }
+             
         }
+
+        static bool NavMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1) Run");
+            Console.WriteLine("2) Exit");
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                return true;
+            }
+            else if (choice == "2")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Brings data in from Json
+        /// </summary>
+        static void GetJson()
+        {
+            string path = "../../../../data.json";
+
+            string data = "";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                data = sr.ReadToEnd();
+            }
+        }
+
+        //TODO: Bring the Json data into the program
+        //TODO: Convert the data into a LINQ
+        //TODO: Output all of the neighborhoods in the data list
+        //TODO: Filter out all the neighborhoods that do not have any names
+        //TODO: Remove the Duplicates
+        //TODO: Rewrite the queries from above, and consolidate all into one single query
+        //TODO: Rewrite at least one of these questions only using the opposing method (example: Use LINQ Query statements instead of LINQ method calls and vise versa.)
+
     }
 }
