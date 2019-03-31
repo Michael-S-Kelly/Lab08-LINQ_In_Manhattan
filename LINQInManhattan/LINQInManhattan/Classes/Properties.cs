79a137d0c837325b1f3b24839795aa0254d87465
Michael Kelly <mike.s.kelly@icloud.com>
2019-03-31 16:17:32 -0700

    Commiting to take a break from trying to fix deserilize issue

--- LINQInManhattan/LINQInManhattan/Classes/Properties.cs
+++ LINQInManhattan/LINQInManhattan/Classes/Properties.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace LINQInManhattan.Classes
 {
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     /// <summary>
     /// Collects the Properties data for PropertyTypes
     /// </summary>
     public class Properties
     {
+        [JsonProperty(PropertyName = "Zip")]
         public string Zip { get; set; }
+        [JsonProperty(PropertyName = "City")]
         public string City { get; set; }
+        [JsonProperty(PropertyName = "State")]
         public string State { get; set; }
+        [JsonProperty(PropertyName = "Address")]
         public string Address { get; set; }
+        [JsonProperty(PropertyName = "Borough")]
         public string Borough { get; set; }
+        [JsonProperty(PropertyName = "Neighborhood")]
         public string Neighborhood { get; set; }
+        [JsonProperty(PropertyName = "County")]
         public string County { get; set; }
     }
 }
--- LINQInManhattan/LINQInManhattan/Program.cs
+++ LINQInManhattan/LINQInManhattan/Program.cs
@@ -13,7 +13,16 @@ namespace LINQInManhattan
         static void Main(string[] args)
         {
             string path = "../../../../data.json";
-            List<PropertyType> raw = GetJson(path);
+            var data = "";
+            
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                data = sr.ReadToEnd();
+            }
+
+            var raw = JsonConvert.DeserializeObject<List<FeatureCollection>>(data);
+
 
             bool menu = true;
             while (menu)
@@ -28,7 +37,7 @@ namespace LINQInManhattan
         /// </summary>
         /// <param name="raw"></param>
         /// <returns></returns>
-        static bool NavMenu(List<PropertyType> raw)
+        static bool NavMenu(List<FeatureCollection> raw)
         {
             Console.Clear();
             Console.WriteLine("1) Print all Properties (unfiltered)");
@@ -79,50 +88,54 @@ namespace LINQInManhattan
             }
         }
 
+        /*
         /// <summary>
         /// Brings data in from Json, Deserializes it, and gives it to the FeatureCollection
         /// </summary>
         /// <param name="path">source file for the data.Json file</param>
         /// <returns>Raw form of the deserialized FeatureCollection</returns>
-        static List<PropertyType> GetJson(string path)
+        static var GetJson(string path)
         {
-            string data = File.ReadAllText(path);
-            List<PropertyType> raw = GetRaw(data);
+            var data = File.ReadAllText(path);
+            var raw = JsonConvert.DeserializeObject<List<FeatureCollection>>(data);
             return raw;
 
         }
+        */
 
-        private static List<PropertyType> GetRaw(string data)
+        /*
+        private static List<RetrieveMultipleResponse> GetRaw(string data)
         {
-            return JsonConvert.DeserializeObject<List<PropertyType>>(data);
+            return JsonConvert.DeserializeObject<List<RetrieveMultipleResponse>>(data);
         }
-
-        static void PrintAllRaw(List<PropertyType> raw)
+        */
+        #region Query methods to execute for options 1 through 5 of NavMenu method
+        static void PrintAllRaw(List<FeatureCollection> raw)
         {
             var data = from feature in raw
-                       select feature.Properties.Neighborhood;
+                       select feature.Features;
         }
 
-        static void PrintKnownNeighborhoods(List<PropertyType> raw)
+        static void PrintKnownNeighborhoods(List<FeatureCollection> raw)
         {
 
         }
 
-        static void PrintKnownNeighborhoodsAlt(List<PropertyType> raw)
+        static void PrintKnownNeighborhoodsAlt(List<FeatureCollection> raw)
         {
 
         }
 
-        static void PrintAllNoDups(List<PropertyType> raw)
+        static void PrintAllNoDups(List<FeatureCollection> raw)
         {
 
         }
 
-        static void PrintKnownNeighborhoodsNoDups(List<PropertyType> raw)
+        static void PrintKnownNeighborhoodsNoDups(List<FeatureCollection> raw)
         {
 
         }
-
+        #endregion
         //TODO: Bring the Json data into the program
         //TODO: Convert the data into a LINQ
         //TODO: Output all of the neighborhoods in the data list
@@ -131,5 +144,23 @@ namespace LINQInManhattan
         //TODO: Rewrite the queries from above, and consolidate all into one single query
         //TODO: Rewrite at least one of these questions only using the opposing method (example: Use LINQ Query statements instead of LINQ method calls and vise versa.)
 
+        /*
+        var data = File.ReadAllText(path);
+        var raw = JsonConvert.DeserializeObject<List<FeatureCollection>>(data);
+        */
+
+        /*
+        var data = "";
+        using (StreamReader sr = File.OpenText(path))
+        {
+            data = sr.ReadToEnd();
+        }
+
+        RootObject root = JsonConvert.DeserializeObject<RootObject>(data);
+        */
+
+        /*
+        var raw = GetJson(path);
+        */
     }
 }
